c2f498498ba4442bd20822a1f3d35c7def2197b4
Julie <julie.albini@edu.univ-fcomte.fr>
2026-01-21 09:06:34 +0100

    Debut skin take order

--- smart-cafe-front/src/components/BaseButton.vue
+++ smart-cafe-front/src/components/BaseButton.vue
@@ -9,13 +9,15 @@ const props = defineProps({
 const variants = {
   primary:
     'bg-(--cafe-secondary) text-(--cafe-primary) hover:bg-(--cafe-primary) hover:text-(--cafe-secondary) hover:border-(--cafe-secondary)',
+  secondary:
+    'bg-(--cafe-primary) text-(--cafe-secondary) border-(--cafe-primary) hover:bg-(--cafe-secondary) hover:text-(--cafe-primary) hover:border-(--cafe-primary)',
 }
 </script>
 
 <template>
   <button
     @click="$emit('click')"
-    class="px-8 py-3 rounded-lg font-medium transition-colors duration-200 ease-in-out border-2 border-transparent cursor-pointer"
+    class="px-8 py-3 rounded-lg font-medium transition-colors duration-200 ease-in-out border-2 cursor-pointer"
     :class="variants[variant]"
   >
     <slot />
